5356104d19a2415b0f0d776df7280f320ef6d828
GreasyRooster <greasyRooster1@gmail.com>
2025-01-01 20:52:11 -0800

    get file

--- src/main.rs
+++ src/main.rs
@@ -34,40 +34,25 @@ fn main() {
             },
 
             (GET) (/stats) => {
-                // If the request's URL is `/panic`, we jump here.
-                //
-                // This block panics. Fortunately rouille will automatically catch the panic and
-                // send back a 500 error message to the client. This prevents the server from
-                // closing unexpectedly.
-                panic!("Oops!")
+                panic!("Not implemented yet")
             },
 
-            (GET) (/{id: u32}) => {
-                // If the request's URL is for example `/5`, we jump here.
-                //
-                // The `router!` macro will attempt to parse the identifier (eg. `5`) as a `u32`. If
-                // the parsing fails (for example if the URL is `/hello`), then this block is not
-                // called and the `router!` macro continues looking for another block.
-                println!("u32 {:?}", id);
+            (GET) (/{uri: String}) => {
+                let host = request.header("Host").unwrap();
+                let path = get_path_from_host(host.to_string(),uri);
+                println!("Requested path: {:?}", path);
 
-                // For the same of the example we return an empty response with a 400 status code.
-                rouille::Response::empty_400()
-            },
 
-            (GET) (/{id: String}) => {
-                // If the request's URL is for example `/foo`, we jump here.
-                //
-                // This route is similar to the previous one, but this time we have a `String`.
-                // Parsing into a `String` never fails.
-                println!("String {:?}", id);
-
-                // Builds a `Response` object that contains "hello, " followed with the value
-                // of `id`.
                 rouille::Response::text(format!("hello, {}", id))
             },
             _ => rouille::Response::empty_404()
         )
     });
+}
+
+fn get_path_from_host(host:String,uri:String)->String{
+    let host_words:Vec<&str> = host.split(".").collect();
+    format!("./data/{1}/{0}{uri}",host_words[0],host_words[1])
 }
 
 // fn main() {
@@ -236,7 +221,3 @@ fn main() {
 //     }
 // }
 //
-// fn get_path_from_host(host:String,uri:String)->String{
-//     let host_words:Vec<&str> = host.split(".").collect();
-//     format!("./data/{1}/{0}{uri}",host_words[0],host_words[1])
-// }
